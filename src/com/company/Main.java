16d9d9bbb7c5ca807d2770391ceacb937eb996fa
rumor1993 <rumor1993@naver.com>
2022-02-23 20:18:57 +0900

    1026: 문제풀이

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,26 @@
 package com.company;
 
-public class Main {
+import java.io.*;
+import java.util.*;
 
-    public static void main(String[] args) {
-	// write your code here
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        String number = bf.readLine();
+
+        char[] numbers = new char[number.length()];
+        for (int i = 0; i < number.length(); i++) {
+            numbers[i] = number.charAt(i);
+        }
+
+        String result = "";
+
+        for (int i = numbers.length; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                result += "0";
+            }
+            System.out.println("[" + result.replaceFirst("0", String.valueOf(numbers[numbers.length-i])) + "]");
+            result = "";
+        }
     }
 }
--- src/com/company/refactoring/ch6_state_pattern/State.java
+++
@@ -1,6 +0,0 @@
-package com.company.refactoring.ch6_state_pattern;
-
-public enum State {
-    STATE_STOPPED,
-    STATE_LOGGING;
-}
