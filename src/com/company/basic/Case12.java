cc7d332c797d00eeb6b5c1876b741702f5884973
rumor1993 <rumor1993@naver.com>
2022-03-08 00:50:05 +0900

    CASE 12

---
+++ src/com/company/basic/Case12.java
@@ -0,0 +1,37 @@
+package com.company.basic;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+// 12. 암호
+public class Case12 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int number = Integer.parseInt(bf.readLine());
+        String str = bf.readLine();
+        List<Character> characters = new ArrayList<>();
+        String answer = "";
+
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == '#') {
+                answer += "1";
+            } else {
+                answer += "0";
+            }
+
+            if (answer.length() == 7) {
+                int binaryToDecimal = Integer.parseInt(answer, 2);
+                char character = (char) binaryToDecimal;
+                characters.add(character);
+                answer = "";
+            }
+        }
+
+        for (char value : characters) {
+            System.out.print(value);
+        }
+    }
+}
